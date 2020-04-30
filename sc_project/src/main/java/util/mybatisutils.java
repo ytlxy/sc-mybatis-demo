725fe1d93ffc680dc936fb5cf8a2ff8da7532f88
ytlxy <a739771409@gmail.com>
2020-04-30 17:37:00 +0800

    up new

--- sc_project/src/main/java/util/mybatisutils.java
+++ sc_project/src/main/java/util/mybatisutils.java
@@ -1,3 +1,5 @@
+package util;
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
--- sc_project/src/test/java/test.java
+++ sc_project/src/test/java/test.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import util.mybatisutils;
 
 import java.util.List;
 
